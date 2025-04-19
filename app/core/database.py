a49a4120dd0294242fd9bf4a33781f1341c5521d
Forhad Ali <85278849+forhad35@users.noreply.github.com>
2025-04-20 00:20:50 +0600

    Update database.py postgresql url

--- app/core/database.py
+++ app/core/database.py
@@ -6,10 +6,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 load_dotenv()  # .env ফাইল লোড করুন
 
-# PostgreSQL URL তৈরি করুন
-POSTGRES_URL = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
 
-engine = create_engine(POSTGRES_URL)
+engine = create_engine(os.getenv('POSTGRES_URL'))
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
 
